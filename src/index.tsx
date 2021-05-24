4965ab9b9f0fbf45fa2de5dfb12fc1a338f9e00d
Rodionov Yaroslav <rodionov.yaroslav2000@gmail.com>
2021-05-24 10:02:36 +0300

    Add fonts and global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import backgroundDown from 'assets/backgroundDown.svg'
+import backgroundDownLow from 'assets/backgroundDownLow.svg'
+import backgroundUp from 'assets/backgroundUp.svg'
+import backgroundUpLow from 'assets/backgroundUpLow.svg'
+import { theme } from './styled'
+import './font.css'
+
+const Global = createGlobalStyle`
+*,
+::after,
+::before{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  overflow-x:hidden;
+  margin-right: calc(-1 * (100vw - 100%));
+  background: url(${backgroundUp}) top center no-repeat, url(${backgroundDown}) bottom center no-repeat #373af5;;
+  background-size: 100%, 100%, cover;
+  @media ${theme.media.phone}{
+    background: url(${backgroundUpLow}) top center no-repeat, url(${backgroundDownLow}) bottom center no-repeat #373af5;
+    background-size: 100%, 100%, cover;
+  }
+}
+`
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
